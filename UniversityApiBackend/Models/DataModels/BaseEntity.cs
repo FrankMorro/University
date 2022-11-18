f1a2f9b80c5c42206c2ca497bccc2414107b7313
JRMartinez <johnny.martinez@outlook.com>
2022-11-18 06:46:22 -0400

    Actualizando Modelo BaaseEntity

--- UniversityApiBackend/Models/DataModels/BaseEntity.cs
+++ UniversityApiBackend/Models/DataModels/BaseEntity.cs
@@ -2,14 +2,16 @@
 
 public class BaseEntity
 {
-    public int Id { get; set; }
+  public int Id { get; set; }
 
-    public string CreatedBy { get; set; } = null!;
-    public DateTime CreatedAt { get; set; } = DateTime.Now;
-    public string? LastModifiedBy { get; set; } = null;
-    public DateTime? LastModified { get; set; } = null;
-    public string? DeletedBy { get; set; } = null;
-    public DateTime? DeletedAt { get; set; } = null;
-    public bool IsDeleted { get; set; } = false;
+  public int UserID { get; set; }
+
+  public virtual User CreatedBy { get; set; } = new User();
+  public DateTime CreatedAt { get; set; } = DateTime.Now;
+  public virtual User? LastModifiedBy { get; set; } = new User();
+  public DateTime? LastModified { get; set; } = null;
+  public virtual User? DeletedBy { get; set; } = new User();
+  public DateTime? DeletedAt { get; set; } = null;
+  public bool IsDeleted { get; set; } = false;
 
 }
